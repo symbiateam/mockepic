53e3662ca22deeb818e18cb36909bcac68aba08f
ashley mo <ashleymo2025@gmail.com>
2024-12-27 19:19:49 -0500

    f

--- src/index.jsx
+++ src/index.jsx
@@ -10,11 +10,10 @@ root.render(
     <App />
   </React.StrictMode>
 )
-
 FHIR.oauth2.authorize({
   clientId: 'whatever',
   scope: 'launch/patient patient/*.write patient/*.read',
   redirectUri: window.location.origin + '/oauth-callback',
-  iss: 'https://launch.smarthealthit.org/v/r4/fhir',
-  fhirServiceUrl: 'https://launch.smarthealthit.org/v/r4/fhir'
+  iss: 'https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImkiOiIxIiwiaiI6IjEifQ/fhir',
+  completeInTarget: true
 });
